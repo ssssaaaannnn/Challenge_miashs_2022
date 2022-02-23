1f10b071317a3032b528787c46002bfa398f4904
RMeuter <r.meuter@hotmail.com>
2022-02-23 16:47:36 +0100

    Model final weighted focal loss

--- models/model_focal_weighted/model_focal_weighted.py
+++ models/model_focal_weighted/model_focal_weighted.py
@@ -23,17 +23,17 @@ from tensorflow.keras.callbacks import LearningRateScheduler
 NAME_MODEL = "RESNET50_PRETRAINED_FOCAL_WEIGHTED"
 
 # VARIABLE DE CHEMIN
-PATH_TRAINSET = "/home/data/challenge_2022_miashs/train"
+PATH_TRAINSET = "D:\Challenge_miashs_2022\data\echantillon_1"
 PATH_TESTSET = "/home/data/challenge_2022_miashs/test"
 PATH_STORAGE_MDOEL = "/home/miashs4/results/"+NAME_MODEL+"/" 
 
 # VARIABLE MODEL
-EPOCH = 30
-BATCH_SIZE = 32
+EPOCH = 20
+BATCH_SIZE = 64
 IMG_H = 200
 IMG_W = 200
 NUM_CLASS = 1081
-weights = np.load(r'/home/miashs4/weights_classes.npy')
+weights = np.load(r'models\model_focal_weighted\weights_classes.npy')
 
 ################################################
 ################################################
@@ -43,6 +43,7 @@ weights = np.load(r'/home/miashs4/weights_classes.npy')
 tf.debugging.set_log_device_placement(False)
 #get list of gpu
 gpus = tf.config.list_physical_devices('GPU')
+print(gpus)
 
 
 try:
@@ -52,14 +53,17 @@ try:
     mixed_precision.set_global_policy('mixed_float16')
 
     #CREATION OF AUGMENTATION
-    TFs = {"horizontal_flip": True,
-        "vertical_flip": True,
-        "rotation_range": 30,
-        "featurewise_std_normalization": True,
-        "brightness_range": (1, 2)
-        }
-
-    datagen = ImageDataGenerator(**TFs)
+    TFs = {#'height_shift_range':  .5,
+       "horizontal_flip": True,
+       "vertical_flip": True,
+       "rotation_range": 30,
+       # "featurewise_std_normalization": True,
+       "brightness_range": (1, 1.5),
+       "shear_range":0.2
+       # other transformation your want
+       # ...
+       }
+    datagen = ImageDataGenerator(**TFs, validation_split=0.2, preprocessing_function=tf.keras.applications.resnet50.preprocess_input)
 
     #GENERATOR CREATION
     validation_generator = datagen.flow_from_directory(
@@ -79,6 +83,20 @@ try:
         target_size=(IMG_H,IMG_W),
         shuffle=True,
         class_mode='binary')
+    
+    #MODEL ADDONS
+    def step_decay(epoch):
+        if epoch < EPOCH/2 + 1:
+            return 10**(-4)
+        elif epoch < EPOCH*2/3 + 1:
+            return 10**(-5)
+        else:
+            return 10**(-6)
+
+    my_callbacks = [
+        tf.keras.callbacks.ModelCheckpoint(filepath='model.{epoch:02d}-{val_loss:.2f}.h5'),
+        LearningRateScheduler(step_decay, verbose=1)
+    ]
 
     #MODELE
 
@@ -97,25 +115,6 @@ try:
     ])
 
 
-    #Fonctionnalité modèle
-    earlyStopping = tf.keras.callbacks.EarlyStopping(
-        monitor="val_loss",
-        min_delta=0,
-        patience=10,
-        verbose=0,
-        mode="auto",
-        baseline=None, # A voir ?
-        restore_best_weights=True,
-    )
-
-    def step_decay(epoch):
-        if epoch < EPOCH/2 + 1:
-            return 10**(-4)
-        elif epoch < EPOCH*2/3 + 1:
-            return 10**(-5)
-        else:
-            return 10**(-6)
-    lrate = LearningRateScheduler(step_decay, verbose=1)
 
     #Modèle compilation
     model.compile(
@@ -127,7 +126,7 @@ try:
     history = model.fit(train_generator, 
         validation_data=validation_generator , epochs=EPOCH,
         batch_size=BATCH_SIZE, verbose=1, validation_steps=1,
-        callbacks=[earlyStopping, lrate]
+        callbacks=my_callbacks
     )
 
     pd.DataFrame(history.history).to_json(PATH_STORAGE_MDOEL+"result_history_resnet.json")
