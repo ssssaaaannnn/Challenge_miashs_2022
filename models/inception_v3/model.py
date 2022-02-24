b05a97e7d866bd145fbdb0a6c84693ffca09ef35
Melody Rous <melody.rous@atos.net>
2022-02-24 11:06:49 +0100

    model inception_v3 et poids des classes

---
+++ models/inception_v3/model.py
@@ -0,0 +1,65 @@
+# !pip install timm 
+import os
+import re
+import pandas as pd
+import numpy as np
+
+import timm
+import timm
+import torch
+from timm.optim.optim_factory import create_optimizer
+from types import SimpleNamespace
+from timm.data.transforms_factory import create_transform
+from timm.data import ImageDataset
+from torch.nn import CrossEntropyLoss
+from torch.optim.lr_scheduler import MultiStepLR
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+
+#Data
+import warnings
+warnings.filterwarnings('ignore', '.*interpolation.*', )
+
+model = timm.create_model('inception_v3', pretrained=True, num_classes=1081)
+# model
+dataset = ImageDataset('/home/data/challenge_2022_miashs/train', 
+                       transform=create_transform(224, is_training=True))
+weights = torch.tensor(np.load("weights_crossentropy.npy"))
+
+loader = DataLoader(dataset, batch_size=96, shuffle=True, num_workers=16)
+
+#Cuda
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+weights.to(device)
+model.to(device)
+
+
+#Modèle
+args = SimpleNamespace()
+args.weight_decay = 1e-4
+args.lr = 0.01
+args.opt = 'sgd' #'lookahead_adam' to use `lookahead`
+args.momentum = 0.9
+
+optimizer = create_optimizer(args, model)
+criterion = CrossEntropyLoss(weight = weights)
+
+scheduler = MultiStepLR(optimizer, milestones=[30, 40], gamma=0.1)
+
+#Epoch
+for epoch in range(25):
+    running_loss = 0
+    for inputs, targets in tqdm(loader):
+        inputs, targets = inputs.to(device), targets.to(device)
+        outputs = model(inputs)
+        loss = criterion(outputs, targets)
+
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+        running_loss += loss.item()
+    print('Loss:', running_loss/len(loader))
+    scheduler.step()
+
+torch.save(model.state_dict(), 'model_inception_v3_cewl.torch')
